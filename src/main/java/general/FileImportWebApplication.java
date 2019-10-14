be35db8fbc26405a6dff9037e3e1636e49356e03
Ula <ulenca2905@gmail.com>
2019-10-14 21:31:59 +0200

    First emty commit

---
+++ src/main/java/general/FileImportWebApplication.java
@@ -0,0 +1,13 @@
+package general;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileImportWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileImportWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/general/FileImportWebApplicationTests.java
@@ -0,0 +1,16 @@
+package general;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileImportWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
