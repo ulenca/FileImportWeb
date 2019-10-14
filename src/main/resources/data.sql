0f28f32c4693c7a4b099733d4843a85bf328feea
Ula <ulenca2905@gmail.com>
2019-10-14 23:08:38 +0200

    Database schema and inserts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+delete from person;
+
+insert into person (first_name, last_name, date_of_birth, phone) values ('Agnieszka', 'Radwańska', '1978-01-01', 789653453);
+insert into person (first_name, last_name, date_of_birth) values ('Radosław', 'Panek', '1982-04-15');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create TABLE if not exists Person (
+
+	id identity,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	date_of_birth date not null,
+	phone int(9)
+	
+);
